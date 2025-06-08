4168f249029a06320cee9d0969a79da62491c9b2
navarrs <ingrid.navarro.an@gmail.com>
2025-06-08 15:44:01 -0400

    added: sample data for testing purposes

--- src/scripts/get_sample.py
+++ src/scripts/get_sample.py
@@ -1,31 +1,44 @@
 import os
 import shutil
-import sys
 import random
+import pickle
 
-def copy_sample_files(input_folder, output_folder, sample_size=10):
-    if not os.path.isdir(input_folder):
-        print(f"Input folder '{input_folder}' does not exist.")
-        return
+if __name__ == "__main__":
+    input_folder = '/data/driving/waymo/new_processed_scenarios_testing'
+    input_meta_file = '/data/driving/waymo/new_processed_scenarios_test_infos.pkl'
+    output_folder = './samples'
+    scenario_folder = f"{output_folder}/scenarios"
+    sample_size = 10
+    os.makedirs(scenario_folder, exist_ok=True)
+    
+    assert os.path.isdir(input_folder), \
+        f"Input folder '{input_folder}' does not exist."
 
-    os.makedirs(output_folder, exist_ok=True)
     files = [f for f in os.listdir(input_folder) if os.path.isfile(os.path.join(input_folder, f))]
-    print(files)
-    if len(files) < sample_size:
-        print(f"Not enough files in input folder. Found {len(files)}, need {sample_size}.")
-        return
+    # print(files)
+    assert len(files) > sample_size, \
+        f"Not enough files in input folder. Found {len(files)}, need {sample_size}."
 
+    assert os.path.exists(input_meta_file), \
+        f"Metadata file '{input_meta_file}' does not exist."
+    with open(input_meta_file, 'rb') as f:
+        metas = pickle.load(f)
+    
+    sample_metas = []
     sample_files = random.sample(files, sample_size)
     for filename in sample_files:
         src = os.path.join(input_folder, filename)
-        dst = os.path.join(output_folder, filename)
-        shutil.copy2(src, dst)
-    print(f"Copied {sample_size} files to '{output_folder}'.")
+        dst = os.path.join(scenario_folder, filename)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python get_sample.py <input_folder> <output_folder>")
-        sys.exit(1)
-    input_folder = sys.argv[1]
-    output_folder = sys.argv[2]
-    copy_sample_files(input_folder, output_folder)
+        scenario_id = filename.split('.')[0].split('_')[-1]
+        for meta in metas:
+            if meta['scenario_id'] == scenario_id:
+                sample_metas.append(meta)
+                break
+
+        shutil.copy2(src, dst)
+
+    with open(os.path.join(output_folder, 'sample_infos.pkl'), 'wb') as f:
+        pickle.dump(sample_metas, f)
+
+    print(f"Copied {sample_size} files to '{output_folder}'.")
