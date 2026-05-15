ad519e64359c391fde25474513a70567eeca277a
navarrs <ingrid.navarro.an@gmail.com>
2026-05-15 13:35:02 -0400

    Added av2 documentation

--- src/characterization/datasets/__init__.py
+++ src/characterization/datasets/__init__.py
@@ -1,5 +1,6 @@
+from .argoverse2 import Argoverse2Data
 from .base_dataset import BaseDataset
 from .nuscenes import NuScenesData
 from .waymo import WaymoData
 
-__all__ = ["BaseDataset", "NuScenesData", "WaymoData"]
+__all__ = ["Argoverse2Data", "BaseDataset", "NuScenesData", "WaymoData"]
